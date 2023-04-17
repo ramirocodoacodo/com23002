7f0f763dc1f812b6160bb74f37fc790e19d21150
Ramiro <ramiro.escalante@bue.edu.ar>
2023-04-17 14:29:45 -0300

    1er commit del repositorio para la com23002.

---
+++ js-1/js/funciones-artimeticas.js
@@ -0,0 +1,45 @@
+num1 = 10;
+num2 = 12;
+// int
+var num3 = 99;  // let
+// float (number)
+// Tipado dinámico
+decimal = 1.23;
+console.log(typeof decimal)
+// string
+decimal = "1.23"
+console.log(typeof decimal)
+// string
+decimal = String(1.23)
+console.log(typeof decimal)
+nombreApellido = "Ramiro Escalante Leiva";
+nombre_apellido = "Ramiro Escalante Leiva";
+logico = true  // false
+// boolean
+console.log(typeof logico)
+
+// Funciones aritméticas
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(num1 % 3);  // 1
+console.log(num1 ** 2);
+console.log(num1 ** (1/3));  // raiz
+
+// console.clear();
+
+// Operador incremental
+i = 0;
+i++;  // i = i + 1;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+
+// Constantes
+const VALOR_PI = 3.14159;
+
+
+// typeof
---
+++ js-1/js/prompt.js
@@ -0,0 +1,7 @@
+console.log("Ingrese los datos del usuario: ");
+// string
+nombre = prompt("Ingrese su nombre: ");
+// int
+edad = parseInt(prompt("Ingrese su edad"));
+console.log("Mi nombre es",nombre, "y mi edad es", edad);
+console.log("Mi nombre es" + nombre + "y mi edad es" + edad);
